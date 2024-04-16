2ba0904fc31c7b05f90e1397bcb96e2798d407d3
Dmitry Putilin <sonicx921@ya.ru>
2024-04-16 13:20:49 +0300

    added tests to view.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {getWeatherByCoordinates, getCityByCoordinates, getCoordinatesByCity} from "./openweathermap.js"
 import {showMap} from "./yamap.js"
+import {showWeather, showCity, makeShowButtonVisible, getCityWeather, addToHistory} from "./view"
+
 function findLocation() {
   if (!navigator.geolocation) {
     console.error("Geolocation isn't work");
@@ -30,94 +32,7 @@ function findLocation() {
   }
 }
 
-function addToHistory(city, lat, lon) {
-  const history = document.getElementById('history');
-  const li = document.createElement('li');
-  li.setAttribute('class', 'list-group-item');
-  const a = document.createElement('a');
-  a.href = '#';
-  a.innerText = city;
-  a.setAttribute('data-lat', lat);
-  a.setAttribute('data-lon', lon);
-  a.onclick = getWeatherFromHistory;
-  li.append(a);
-  history.append(li);
-  const list = history.querySelectorAll('li.list-group-item:not(li.active)');
-  if (list.length > 5) history.removeChild(list[0]);
-}
 
-function showCity(city) {
-  const cityHeader = document.getElementById('city');
-  cityHeader.innerText = city;
-}
-function showWeather(current, min, max, icon) {
-  const weatherField = document.getElementById('weather');
-  const oldPar = weatherField.querySelector('p');
-  weatherField.removeChild(oldPar);
-  const oldImg = weatherField.querySelector('img');
-  if (oldImg) weatherField.removeChild(oldImg);
-  const img = document.createElement('img');
-  img.src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
-  const p = document.createElement('p');
-  p.innerHTML = `Current: <b>${Math.round(current)}</b><br>`;
-  p.innerHTML += `Max: <b>${Math.round(max)}</b><br>`;
-  p.innerHTML += `Min: <b>${Math.round(min)}</b><br>`;
-  weatherField.append(p);
-  weatherField.append(img);
-}
-
-function makeShowButtonVisible() {
-  const input = document.querySelector('#input').value;
-  if (input !== '') {
-    const but = document.querySelector('button[type="submit"]');
-    but.removeAttribute('disabled');
-  } else {
-    document
-      .querySelector('button[type="submit"]')
-      .setAttribute('disabled', '');
-  }
-}
-
-function getCityWeather() {
-  const input = document.querySelector('#input');
-  const city = input.value;
-  if (city !== '') {
-    getCoordinatesByCity(city)
-      .then((data) => {
-        if (data.length === 0) {
-          alert('Incorrect city name!');
-        } else {
-          addToHistory(data[0].name, data[0].lat, data[0].lon);
-          showMap(data[0].lat, data[0].lon);
-          showCity(data[0].name);
-          getWeatherByCoordinates(data[0].lat, data[0].lon)
-              .then((data) => {
-                showWeather(data.main.temp, data.main.temp_min,
-                    data.main.temp_max, data.weather[0].icon);
-              });
-        }
-      })
-  }
-
-  input.value = '';
-  const button = document.getElementById('show');
-  button.setAttribute("disabled", '');
-}
-
-function getWeatherFromHistory(event) {
-  let lat = event.target.getAttribute('data-lat');
-  let lon = event.target.getAttribute('data-lon');
-  getWeatherByCoordinates(lat, lon).then(data => {
-    showMap(lat, lon);
-    showCity(event.target.innerText);
-    showWeather(
-        data.main.temp,
-        data.main.temp_min,
-        data.main.temp_max,
-        data.weather[0].icon,
-    );
-  });
-}
 
 window.addEventListener('load', findLocation);
 const button = document.getElementById('show');
---
+++ src/view.test.js
@@ -0,0 +1,101 @@
+import {showCity, makeShowButtonVisible, getWeatherFromHistory, addToHistory, showWeather} from "./view.js"
+
+beforeAll(() => {
+    document.body.innerHTML = `<main>
+            <div className="row g-5">
+                <div className="col-md-8">
+                    <form className="row g-3" onSubmit="return false;">
+                        <div className="col input-group mb-3">
+                            <input id="input" type="text" className="form-control"
+                                placeholder="Weather"
+                                aria-label="Username"
+                                aria-describedby="basic-addon1"
+                                />
+                        </div>
+                        <div className="col">
+                            <button
+                                id="show"
+                                type="submit"
+                                className="btn btn-primary mb-3"
+                                disabled
+                            >
+                                Show
+                            </button>
+                        </div>
+                    </form>
+                    <div className="row row-cols-2">
+                        <div className="col">
+                            <img
+                                id="map"
+                                src="map.png"
+                                className="img-fluid rounded p-1"
+                                alt="map"
+                            />
+                        </div>
+                        <div className="col" id="weather"><h2 id="city">Weather info</h2><p id="temp">Current: <b></b><br/>Max: <b></b><br/>Min: <b></b><br/></p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <ul className="list-group" id="history">
+                        <li className="list-group-item active" aria-current="true">
+                            History </li>
+                    </ul>
+                </div>
+            </div>
+        </main>`;
+});
+
+it('should show city in header', () => {
+    // const div = document.createElement('div');
+    // div.setAttribute('id', 'weather');
+    // const h2 = document.createElement('h2');
+    // h2.setAttribute('id', 'city');
+    // h2.innerText = 'Weather info';
+    // div.appendChild(h2);
+    // document.body.append(div);
+    showCity('Moscow');
+    expect(document.querySelector('#city').innerText).toEqual('Moscow');
+});
+
+it('should make show button visible', () => {
+    const event = new Event('change');
+    const input = document.getElementById('input');
+    input.addEventListener('change', makeShowButtonVisible);
+    input.value = 'SomeCity';
+    input.dispatchEvent(event);
+    expect(document.querySelector('#show').hasAttribute('disabled')).toBeFalsy();
+});
+
+it('should make show button invisible', () => {
+    const event = new Event('change');
+    const input = document.getElementById('input');
+    input.addEventListener('change', makeShowButtonVisible);
+    input.value = '';
+    input.dispatchEvent(event);
+    expect(document.querySelector('#show').hasAttribute('disabled')).toBeTruthy();
+});
+
+it('should add city and lat lot to history', () => {
+    addToHistory('Moscow', 100, 200);
+    const history = document.getElementById('history');
+    const list = history.querySelector('li.list-group-item:not(li.active)');
+    console.log(list);
+    expect(list.innerHTML).toBe('<a href="#" data-lat="100" data-lon="200">Moscow</a>');
+});
+
+it('should add temp to the fields', () => {
+    showWeather(20, 10, 30, '10d')
+    const weather = document.querySelector('#weather > p');
+    console.log(weather);
+    expect(weather.innerHTML)
+        .toBe('Current: <b>20</b><br>Max: <b>30</b><br>Min: <b>10</b><br>');
+});
+
+it('should add weather picture', () => {
+    showWeather(20, 10, 30, '10d')
+    const img = document.querySelector('#weather > img');
+    console.log(img);
+    expect(img.src).toBe('https://openweathermap.org/img/wn/10d@4x.png');
+});
+
